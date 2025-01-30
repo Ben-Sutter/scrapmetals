251f5d47eb0ed84d123a5d4345b265ef27f4f0df
ben-sutter <42955074+Ben-Sutter@users.noreply.github.com>
2025-01-30 03:36:35 +0000

    changed over to styled email version

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HoldingPage from "./pages/HoldingPage";
 import WorkInProgress from "./pages/WorkInProgress";
-import EmailHolding from "./pages/WIPemailHolding";
+import EmailHolding from "./pages/EmailHolding";
 const App = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<HoldingPage />} />
+          <Route path="/" element={<EmailHolding />} />
+          <Route path="/old" element={<HoldingPage />} />
           <Route path="/wip-api" element={<WorkInProgress />} />
-          <Route path="/wip-email" element={<EmailHolding />} />
         </Routes>
       </div>
     </Router>
